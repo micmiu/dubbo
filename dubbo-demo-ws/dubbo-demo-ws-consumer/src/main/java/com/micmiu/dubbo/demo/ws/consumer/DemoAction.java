2ef980c223370e1189fe6fcb8fd329d6ebea7fa1
micmiu <sjsky007@gmail.com>
2015-10-28 16:04:41 +0800

    Add webservice demo project

---
+++ dubbo-demo-ws/dubbo-demo-ws-consumer/src/main/java/com/micmiu/dubbo/demo/ws/consumer/DemoAction.java
@@ -0,0 +1,33 @@
+package com.micmiu.dubbo.demo.ws.consumer;
+
+import com.micmiu.dubbo.demo.ws.DemoService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created
+ * User: <a href="http://micmiu.com">micmiu</a>
+ * Date: 10/13/2015
+ * Time: 14:26
+ */
+public class DemoAction {
+
+	private DemoService demoService;
+
+	public void setDemoService(DemoService demoService) {
+		this.demoService = demoService;
+	}
+
+	public void start() throws Exception {
+		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+			try {
+				String hello = demoService.sayHello("micmiu.com" + i);
+				System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] " + hello);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Thread.sleep(2000);
+		}
+	}
+}
---
+++ dubbo-demo-ws/dubbo-demo-ws-provider/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: micmiu
+  Date: 10/14/2015
+  Time: 16:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>demo-ws</title>
+</head>
+<body>
+webservice demo run on dubbo.
+</body>
+</html>
